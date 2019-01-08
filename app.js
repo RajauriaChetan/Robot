e233a31d068441d684b1428a9fcd312b26338926
Chetan Rajauria <chetanrajauria@ishait.delhi.carzonrent.com>
2019-01-08 11:32:36 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//app.get('/', (req, res) => res.send('My Name is Chetan Rajauria.'))
+
+const dialogflow = require('dialogflow');
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+    res.sendFile('Index.html', { root: __dirname });
+});
+
+function ultaseedhamethod(greet)
+{
+    console.log(greet);
+}
+
+ultaseedhamethod("cHeTaN")
+app.listen(port, () => console.log('Example app listening on port ${port}!'))
+
+
+
+
+
+
+
+
+
+
+
