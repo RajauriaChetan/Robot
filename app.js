ce19539162576b3ee2806e0f56017f817d085b23
Chetan Rajauria <chetan.r@mylescars.com>
2019-01-09 15:35:07 +0530

    Changes done at 4:23

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const port = process.env.PORT || 8080;
 
 //app.get('/', (req, res) => res.send('My Name is Chetan Rajauria.'))
 
-const dialogflow = require('dialogflow');
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,36 +19,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
     app.post('', function (req, res){
-        var objhhhhhh;
 
-        console("The Intent name is : ",res.body.queryResult.intent.displayName)
-
-        console.log("The value is :",res.body.queryResult.fulfillmentText
-)
+        
         if (req.body.queryResult.intent.displayName.trim() == "home") {
 
         console.log("The endpoint is: " + JSON.stringify(req.body, null, 0));
 
-        res.body.queryResult.intent.fulfillmentText = "Chetan Rajauria";
-
-        objhhhhhh = {
+        var obj = {
             "fulfillmentText": "I am going to Delhi."
         }
-        res.status(201).json(res.body.queryResult.intent.fulfillmentText);
+        res.status(201).json(obj);
+
+        }else{
+        var obj = {
+            "fulfillmentText": "Can you rephrase what you just said?!"
+        }
+        res.status(201).json(obj);
+        
         }
     });
 
 
 
-    app.listen(port, () => console.log('Example app listening on port ${port}!'))
+    app.listen(port);
 
-
-
-
-
-
-
-
+    console.log("This Appjs is live!")
 
 
 
