884c3596808c1977cc2bdd564e0481dc1372e37e
Chetan Rajauria <chetan.r@mylescars.com>
2019-01-14 15:41:44 +0530

    Committed on 11:13

--- app.js
+++ app.js
@@ -15,24 +15,43 @@ app.use(bodyParser.json());
     console.log("The endpoint is: " + JSON.stringify(req.body, null, 0));
 
 
-    var parametersArr = req.body.queryResult.parameters.userpolicy;
+    var parametersArr = req.body.queryResult.parameters.PolicyPrice;
     var obj;
     console.log("Params Arrrrrrrrrrrrrr: ",parametersArr);
 
-
         if (req.body.queryResult.intent.displayName.trim() == "Policy") {
 
-    var parametersArr = req.body.queryResult.parameters.userpolicy;
+    var parametersArr = req.body.queryResult.parameters.PolicyPrice;
     var parametersLength = parametersArr.length;
     var obj;
     console.log("Params Arr: ",parametersArr);
 
-    if (parametersArr[0].trim() == "kms")
+    if (parametersArr[0].trim() == "kms" || parametersArr[0].trim() == "unlimited" || parametersArr[0].trim() == "rates" || parametersArr[0].trim() == "book")
     {
         obj = {
-            "fulfillmentText": "Oh Hi! My name is Alexa. How can I help you?"
+            "fulfillmentText": "We currently offer 3 packages for 120 km, 240 km and Unlimited"
         }
     }
+    else if (parametersArr[0].trim() == "packages" || parametersArr[0].trim() == "different")
+    {
+        obj = {
+            "fulfillmentText": "In 120 Km & 240 Km Additional km driven on these two packages will be charged at Rs. 11, 12 and 14 per km (depending on car category)"
+        }
+    }
+    else if (parametersArr[0].trim() == "minimum")
+    {
+        obj = {
+            "fulfillmentText": "Ahhh, The minimum offer is we currently offer 3 packages for 120 km, 240 km and Unlimited"
+        }
+    }
+    else
+    {
+        obj = {
+            "fulfillmentText": "I am trying very hard to learn it."
+        }
+    }
+
+    //
         res.status(201).json(obj);
         }
     });
